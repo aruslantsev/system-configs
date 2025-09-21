d10366f35dc72db79c9cf5ee183965d5f59855ab
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-09-21 14:51:38 +0300

    Updated make_uki script

--- Gentoo/ExpertBook/kernel/make_uki.sh
+++ Gentoo/ExpertBook/kernel/make_uki.sh
@@ -1,5 +1,17 @@
-dracut --uefi --no-hostonly --kver ${KVER}
-mv /boot/EFI/Linux/linux-${KVER}-*.efi /boot/linux-${KVER}.efi.orig
-cd /boot
-sbsign --key ~/secureboot/db.key --cert ~/secureboot/db.crt --output /boot/linux-${KVER}.efi /boot/linux-${KVER}.efi.orig
-gpg --batch --detach-sign linux-${KVER}.efi
+if [ -z "${1}" ]; then
+	echo "Usage: ${0} KVER"
+	exit 1
+fi
+
+if [ $(whoami) != 'root' ]; then
+	echo "You are not root"
+	exit 1
+fi
+
+KVER="${1}"
+dracut --kver ${KVER} \
+	&& mv /boot/EFI/Linux/linux-${KVER}-*.efi /boot/linux-${KVER}.efi.orig \
+	&& cd /boot \
+	&& sbsign --key ~/secureboot/db.key --cert ~/secureboot/db.crt --output linux-${KVER}.efi linux-${KVER}.efi.orig \
+	&& gpg --detach-sign linux-${KVER}.efi \
+	&& echo "Success" || echo "Failed"
--- Gentoo/Homeserver/kernel/make_uki.sh
+++ Gentoo/Homeserver/kernel/make_uki.sh
@@ -1,1 +1,14 @@
-dracut -o crypt --no-hostonly --kver ${KVER} --uefi
+if [ -z "${1}" ]; then
+	echo "Usage: ${0} KVER"
+	exit 1
+fi
+
+if [ $(whoami) != 'root' ]; then
+	echo "You are not root"
+	exit 1
+fi
+
+KVER="${1}"
+dracut --kver ${KVER} \
+	&& mv /boot/EFI/Linux/linux-${KVER}-*.efi /boot/linux-${KVER}.efi.orig \
+	&& echo "Success" || echo "Failed"
